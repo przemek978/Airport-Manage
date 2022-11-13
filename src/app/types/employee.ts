b5aeacadb7e784ab00f38e679fcab404156e16eb
PKuczynski <przemek.ku2000@gmail.com>
2022-11-13 16:43:33 +0100

    add componets and modified flight

---
+++ src/app/types/employee.ts
@@ -0,0 +1,5 @@
+export class employee{
+  constructor(private name:string,private surname:string,private position:string ){
+
+  }
+}
--- src/app/types/flight.ts
+++ src/app/types/flight.ts
@@ -1,36 +1,42 @@
 import { Passenger } from "./passenger"
+import { Pilot } from "./pilot";
+import { Plane } from "./plane";
 
 export class Flight {
-  //private ticket:Ticket;
+
   constructor(
-    private plane: string,
+    private plane: Plane,
     private passengers: Passenger[],
-    private pilots: string,
+    private pilots: Pilot[],
     private data: Date
   ) {}
 
   public addpassager(pass:Passenger){
     this.passengers.push(pass);
   }
-  get Plane(): string {
+  public addpilots(pilot1:Pilot,pilot2:Pilot){
+    this.pilots.push(pilot1);
+    this.pilots.push(pilot2);
+  }
+  get Plane(): Plane {
     return this.plane;
   }
   get Passengers(): Passenger[] {
     return this.passengers;
   }
-  get Pilots(): string {
+  get Pilots(): Pilot[] {
     return this.pilots;
   }
   get Data(): Date {
     return this.data;
   }
-  set Plane(p: string) {
+  set Plane(p: Plane) {
     this.plane = p;
   }
   set Passengers(Passengers: Passenger[]) {
     this.passengers = this.passengers;
   }
-  set Pilots(pilots: string) {
+  set Pilots(pilots: Pilot[]) {
     this.pilots = pilots;
   }
   set Data(data: Date) {
