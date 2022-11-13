ad2738b239891ad40a55d6cdd183ab5ba35a2dee
PKuczynski <przemek.ku2000@gmail.com>
2022-11-13 15:51:07 +0100

    1311 add basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PassengerComponent } from './passenger/passenger.component';
+import { PassengersComponent } from './passengers/passengers.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'comp1', component: PassengersComponent },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
