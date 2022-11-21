a2569e11455bd2808ea8b5c12477a304b0970689
przemek978 <przemek.ku2000@gmail.com>
2022-11-21 20:54:19 +0100

    oo

--- src/app/addpassengertofly/addpassengertofly.component.ts
+++ src/app/addpassengertofly/addpassengertofly.component.ts
@@ -1,6 +1,9 @@
 import { Component, Inject, Input, OnInit } from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import { Flight } from '../types/flight';
 import { Passenger } from '../types/passenger';
+import { Pilot } from '../types/pilot';
+import { Plane } from '../types/plane';
 
 @Component({
   selector: 'app-addpassengertofly',
@@ -8,8 +11,19 @@ import { Passenger } from '../types/passenger';
   styleUrls: ['./addpassengertofly.component.css']
 })
 export class AddpassengertoflyComponent implements OnInit {
+  passengers: Passenger[]=[ new Passenger("Jan","Kowalski",517355566,"24-12-2000"),
+  new Passenger("Adam","Nowak",606232556,"12-05-1984"),
+  new Passenger("Andrzej","Malinowski",506243445,"30-09-1996"),
+  new Passenger("Anna","Wiśniewska",786345322,"19-01-1967")];
 
-  //@Input() data!:Passenger;
+  pilots: Pilot[]=[ new Pilot("Franek","Nowal", 1),
+  new Pilot("Sebastian","Kowal", 2),];
+
+  planes: Plane[]=[new Plane("Samolot1",100),new Plane("Samolot2",200)];
+
+  flights: Flight[]=[ new Flight(1,this.planes[0], this.passengers, this.pilots, new Date("2000-01-21")),
+  new Flight(2,this.planes[1], this.passengers, this.pilots, new Date("2001-01-21"))];
+    //@Input() data!:Passenger;
   constructor(private dialogRef: MatDialogRef<AddpassengertoflyComponent>, @Inject(MAT_DIALOG_DATA) public data: Passenger) { }
 
   ngOnInit(): void {
