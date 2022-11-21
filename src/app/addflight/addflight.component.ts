66ffa2ae4e21cca8d8f9a5fc041f9ed16824a7ef
arekw20 <arekw90@gmail.com>
2022-11-21 21:20:17 +0100

    aa

--- src/app/addflight/addflight.component.ts
+++ src/app/addflight/addflight.component.ts
@@ -1,6 +1,9 @@
 import { Component, Inject, Input, OnInit } from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import { Flight } from '../types/flight';
+import { Passenger } from '../types/passenger';
+import { Pilot } from '../types/pilot';
+import { Plane } from '../types/plane';
 
 
 
@@ -11,10 +14,27 @@ import { Flight } from '../types/flight';
 })
 export class AddflightComponent implements OnInit {
 
+  passengers: Passenger[]=[ new Passenger("Jan","Kowalski",517355566,"24-12-2000"),
+
+  new Passenger("Adam","Nowak",606232556,"12-05-1984"),
+  new Passenger("Andrzej","Malinowski",506243445,"30-09-1996"),
+  new Passenger("Anna","Wiśniewska",786345322,"19-01-1967")];
+
+  pilots: Pilot[]=[ new Pilot("Franek","Nowal", 1),
+  new Pilot("Sebastian","Kowal", 2),];
+
+  planes: Plane[]=[new Plane("Samolot1",100),new Plane("Samolot2",200)];
+
+  flights: Flight[]=[ new Flight(1,this.planes[0], this.passengers, this.pilots, new Date("2000-01-21")),
+  new Flight(2,this.planes[1], this.passengers, this.pilots, new Date("2001-01-21"))];
+
   constructor(private dialogRef: MatDialogRef<AddflightComponent>, @Inject(MAT_DIALOG_DATA) public data: Flight) { }
 
   ngOnInit(): void {}
   onNoClick() {
     this.dialogRef.close();
   }
+
+
+
 }
