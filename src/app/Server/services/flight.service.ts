a384174d09913fc0d4b52fdfd4432c4a8266c45f
przemek978 <przemek.ku2000@gmail.com>
2022-11-23 22:04:23 +0100

    ll

--- src/app/Server/services/flight.service.ts
+++ src/app/Server/services/flight.service.ts
@@ -4,10 +4,9 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class ApiService {
+export class FlightService {
 
   constructor(private http:HttpClient) { }
-
   postFlight(data:any){
     return this.http.post<any>("http://localhost:3000/flights/",data);
   }
---
+++ src/app/Server/services/plane.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlaneService {
+
+  constructor(private http:HttpClient) { }
+  postPlane(data:any){
+    return this.http.post<any>("http://localhost:3000/planes/",data);
+  }
+  getPlane(){
+    return this.http.get<any>("http://localhost:3000/planes/");
+  }
+}
