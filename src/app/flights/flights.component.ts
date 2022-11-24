c267ab8e3fca48a70c707cf1d0aa16e3a6b48640
arekw20 <arekw90@gmail.com>
2022-11-24 21:07:06 +0100

    ..

--- src/app/flights/flights.component.ts
+++ src/app/flights/flights.component.ts
@@ -1,13 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { AddflightComponent } from '../addflight/addflight.component';
 import { EditflightComponent } from '../editflight/editflight.component';
 import { FlightService } from '../Server/services/flight.service';
 import { Flight } from '../types/flight';
 import { Passenger } from '../types/passenger';
-import { Pilot } from '../types/pilot';
-import { Plane } from '../types/plane';
 
 @Component({
   selector: 'app-flights',
@@ -16,14 +14,6 @@ import { Plane } from '../types/plane';
 })
 export class FlightsComponent implements OnInit {
 
-
-  // passengers: Passenger[]=[ new Passenger("Jan","Kowalski",517355566,"24-12-2000",1),
-  //                           new Passenger("Adam","Nowak",606232556,"12-05-1984",2),
-  //                           new Passenger("Andrzej","Malinowski",506243445,"30-09-1996",3),
-  //                           new Passenger("Anna","Wiśniewska",786345322,"19-01-1967",4)];
-
-  // flights: Flight[]=[ new Flight(1,new Plane(1,"Samolot1",100), new Pilot("Franek","Nowal", 1), new Pilot("Sebastian","Kowal", 2), new Date("2000-01-21")),
-  //                     new Flight(2,new Plane(2,"Samolot2",200), new Pilot("Franek","Nowal", 1), new Pilot("Sebastian","Kowal", 2), new Date("2001-01-21"))];
   passengers: Passenger[]=[];
   flights: Flight[]=[];
   private _selectedFlight!:Flight;
