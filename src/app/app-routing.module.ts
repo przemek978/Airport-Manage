09ebbc36c4bd40db75c81cd89cb5f73bbb2e6bd6
arekw20 <arekw90@gmail.com>
2022-11-24 22:48:35 +0100

    a

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AddflightComponent } from './addflight/addflight.component';
 import { AddpassengertoflyComponent } from './addpassengertofly/addpassengertofly.component';
 import { FlightsComponent } from './flights/flights.component';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { PassengerComponent } from './passenger/passenger.component';
 import { PassengersComponent } from './passengers/passengers.component';
 import { PlanesComponent } from './planes/planes.component';
 
-const routes: Routes = [{ path: 'planes', component: PlanesComponent },{ path: 'flights', component: FlightsComponent },{ path: 'login', component: LoginComponent },{ path: 'passengers/:id', component: PassengersComponent }];
+const routes: Routes = [{ path: 'home', component: HomeComponent }, { path: 'planes', component: PlanesComponent },{ path: 'flights', component: FlightsComponent },{ path: 'login', component: LoginComponent },{ path: 'passengers/:id', component: PassengersComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
