393d94eeb1ec9156c383d932e27e48cddf7f91a9
przemek978 <przemek.ku2000@gmail.com>
2022-11-25 20:47:24 +0100

    dfjd

--- src/app/passengers/passengers.component.ts
+++ src/app/passengers/passengers.component.ts
@@ -19,6 +19,7 @@ export class PassengersComponent implements OnInit {
   selectedPassenger!:Passenger;
   private newPass:Passenger=new Passenger("","",0,"",0);
   flight!:Flight;
+  temp!:any;
   constructor(public dialog: MatDialog,private flightservice:FlightService,private route: ActivatedRoute) {
 
   }
@@ -26,9 +27,15 @@ export class PassengersComponent implements OnInit {
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => this.flightid = Number(params.get('id')));
     //this.passengers=this.flight.passengers;
-    this.flightservice.getFlightid(this.flightid).subscribe(res=>{
+    this.flightservice.getFlightid(this.flightid)
+    .subscribe(res=>{
           this.flight=res;
-          console.log(res);});
+          // this.temp=this.flight;
+          // this.flight.passengers=[];
+          // for(let i=0;i<this.temp.passnger;i++){
+          //   this.flight.passengers.push(new Passenger(this.temp.passengers._name,this.temp.passengers._surname,this.temp.passengers._phone_nr,this.temp.passengers._birthday,this.temp.passengers._id));
+          // }
+    });
 
   }
 
@@ -45,7 +52,7 @@ export class PassengersComponent implements OnInit {
     if (add) {
       dialogRef = this.dialog.open(AddpassengertoflyComponent, {
         width: '30%',
-        data: this.newPass
+        data: {pass: this.newPass,flight: this.flight}
         // {name: this.newPass.name ,surname: 'a',phone: '',date: ''}
       })
     } else if (edit) {
@@ -57,28 +64,35 @@ export class PassengersComponent implements OnInit {
     } else
       return;
       dialogRef.afterClosed().subscribe(result => {
-        console.log(result.data);
+        //console.log(result.data);
         if (result !== undefined) {
 
           if (result.data.name.length <3) {
               alert("Za krótkie imie")
           }
           if(result.data.id== undefined){
-            result.id=this.passengers.length+1;
+            result.data.id=this.passengers.length+1;
           }
           if(add){
-            if(this.flight.passengers==undefined){
-              this.flight.passengers=[];
-            }
-            this.flight.passengers.push(new Passenger(result.data.name,result.data.surname,result.data.phone,result.data.birth,this.flight.passengers.length+1));
-            this.flightservice.putFlight(this.flight,this.flightid).subscribe({
-              next: (res) => {
-                alert("Dodano pasażera")
-              },
-              error: () => {
-                alert("Wystąpił błąd");
-              }
-            });
+
+           // console.log(result);
+          //  this.newPass=new Passenger(result.data.name,result.data.surname,result.data.phone_nr,result.data.birthday,this.flight.passengers.length+1);
+          //  // this.newPass.id=this.flight.passengers.length+1;
+          //  // this.newPass.name=result.data.name;
+          //  // this.newPass.surname=result.data.surname;
+          //  // this.newPass.phone_nr=result.data.phone_nr;
+          //  // this.newPass.birthday=result.data.birthday;
+          //  console.log(this.newPass);
+          //  this.flight.passengers.push(this.newPass);
+          //  console.log(this.flight);
+          //  this.flightservice.putFlight(this.flight,this.flightid).subscribe({
+          //    next: (res) => {
+          //      alert("Dodano pasażera")
+          //    },
+          //    error: () => {
+          //      alert("Wystąpił błąd");
+          //    }
+          //  });
           }
           else if(edit){
             this.passengers.forEach((obj, index, tab) =>{
