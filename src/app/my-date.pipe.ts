7b2bbb7e047b1afc5f05f145d314c8fa153ec4a3
przemek978 <przemek.ku2000@gmail.com>
2022-11-27 22:05:51 +0100

    sdh

--- src/app/my-date.pipe.ts
+++ src/app/my-date.pipe.ts
@@ -6,20 +6,20 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class MyDatePipe extends DatePipe implements PipeTransform {
 
-  override transform(value: any, ...args: any[]): any {
-    // const date = super.transform(value, 'medium');;
-    // const dateTab = date.split('-');
-    // let datePlForm = '';
-    // switch (dateTab[1]) {
-    //   case '01': dateTab[1] = 'stycznia'; break;
-    //   case '02': dateTab[1] = 'lutego'; break;
-    //   case '03': dateTab[1] = 'marca'; break;
-    // }
-    // dateTab.forEach((el, ind) => {
-    //   if (ind === 2) { el += ' godzina'; }
-    //   datePlForm += el + ' ';
-    // });
-    // return datePlForm;
-  }
+  // transform(value: Date, ...args: any[]): any {
+  //   const date = super.transform(value,'dd MMMM YYYY, HH:MM');;
+  //   const dateTab = date.split(' ');
+  //   let datePlForm = '';
+  //   switch (dateTab[1]) {
+  //     case '01': dateTab[1] = 'stycznia'; break;
+  //     case '02': dateTab[1] = 'lutego'; break;
+  //     case '03': dateTab[1] = 'marca'; break;
+  //   }
+  //   dateTab.forEach((el, ind) => {
+  //     if (ind === 2) { el += ' godzina'; }
+  //     datePlForm += el + ' ';
+  //   });
+  //   return datePlForm;
+  // }
 
 }
