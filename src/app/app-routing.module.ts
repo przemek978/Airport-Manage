6f48fb216ba940ecee2ed92a2296b66a86c93731
przemek978 <przemek.ku2000@gmail.com>
2022-11-27 18:50:51 +0100

    kk

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ import { PassengersComponent } from './passengers/passengers.component';
 import { PlanesComponent } from './planes/planes.component';
 //import { LoginDirective } from './login.directive';
 
-const routes: Routes = [ {path: '', redirectTo: 'home', pathMatch: 'full' },{ path: 'home', component: HomeComponent },{ path: 'logout', component: HomeComponent } ,{ path: 'planes', component: PlanesComponent },{ path: 'flights', component: FlightsComponent },{ path: 'login', component: LoginComponent },{ path: 'passengers/:id', component: PassengersComponent }];
+const routes: Routes = [ {path: '', redirectTo: 'home', pathMatch: 'full' },{ path: 'home', component: HomeComponent } ,{ path: 'planes', component: PlanesComponent },{ path: 'flights', component: FlightsComponent },{ path: 'login', component: LoginComponent },{ path: 'passengers/:id', component: PassengersComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
